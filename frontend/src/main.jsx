49975c4eb0801f2e7fbdc9a48042050a2879209e
alimuhammad-007 <alimuhammadashraf75@gmail.com>
2026-05-31 02:42:32 +0500

    Fix casing error for App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import App from './app.jsx'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
